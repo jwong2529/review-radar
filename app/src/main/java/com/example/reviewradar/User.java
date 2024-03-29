3e36bab0b4e6e2ff25b2c2a6027b1fd0775e865c
RuiChai_1102 <87892821+ChaiRui1102@users.noreply.github.com>
2024-03-29 15:25:08 -0700

    3

---
+++ app/src/main/java/com/example/reviewradar/User.java
@@ -0,0 +1,50 @@
+package com.example.reviewradar;
+
+public class User {
+    private int id;
+    private String username;
+    private String email;
+
+    // Constructor
+    public User(int id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // toString() method for debugging
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
