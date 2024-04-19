e68d705a4c681be75ffd44506a18b2625e630fdd
tiffany-mo <motiffany123@gmail.com>
2024-04-18 20:07:30 -0700

    authentication w/firebase

--- app/src/main/java/com/example/reviewradar/CreateAccount.java
+++ app/src/main/java/com/example/reviewradar/CreateAccount.java
@@ -9,32 +9,67 @@ import android.widget.Toast;
 
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import android.util.Log;
+
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 public class CreateAccount extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private static final String TAG = "CustomAuthActivity";
+    private String mCustomToken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
-
+        mAuth = FirebaseAuth.getInstance();
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
 
 
         MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
 
-        loginbtn.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                    Toast.makeText(CreateAccount.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(CreateAccount.this, "Login Failed!", Toast.LENGTH_SHORT).show();
-                }
-            }
+    }
 
-        });
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    private void startSignIn() {
+        // [START sign_in_custom]
+        mAuth.signInWithCustomToken(mCustomToken)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCustomToken:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                            Toast.makeText(CreateAccount.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END sign_in_custom]
+    }
 
+    private void updateUI(FirebaseUser user) {
 
     }
 
