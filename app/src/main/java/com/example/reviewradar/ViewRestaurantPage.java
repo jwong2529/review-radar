6aa2d20d08a3f3f2534d2139b0c8c6cd2d12fa87
jwong2529 <janice.wong2529@gmail.com>
2024-04-19 00:21:35 -0700

    add restaurant page

--- app/src/main/java/com/example/reviewradar/ViewRestaurantPage.java
+++ app/src/main/java/com/example/reviewradar/ViewRestaurantPage.java
@@ -1,10 +1,11 @@
 package com.example.reviewradar;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class ViewRestaurantPage extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
