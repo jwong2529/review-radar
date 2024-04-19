d4accf05928e4eb68c3fd9043fd067e5d50f15c6
tiffany-mo <motiffany123@gmail.com>
2024-04-19 14:05:09 -0700

    redirect code in main activity using navbar

--- app/src/main/java/com/example/reviewradar/MainActivity.java
+++ app/src/main/java/com/example/reviewradar/MainActivity.java
@@ -21,16 +21,51 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
+
+
+
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RestaurantAdapter adapter;
     private Map<String, String> restaurantMap;
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            if (item.getItemId() == R.id.home) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                return true;
+            } else if (item.getItemId() == R.id.profile) {
+                startActivity(new Intent(MainActivity.this, CreateAccount.class));
+                return true;
+            } else if (item.getItemId() == R.id.post_nav) {
+                startActivity(new Intent(MainActivity.this, PostAReview.class));
+                return true;
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.home_page);
+
+        BottomNavigationView navigation = findViewById(R.id.bottomNavigationView);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         //Parse csv file to obtain restaurant data
         try {
